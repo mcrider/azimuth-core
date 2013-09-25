55f073678119663f3264792370999f856a490071
Matt Crider <mattcrider@gmail.com>
2013-09-24 22:14:51 -1000

    Completed routes and bumped version

--- client/controllers.js
+++ client/controllers.js
@@ -3,9 +3,66 @@ PageController = RouteController.extend({
   run: function() {
     var page_slug = this.params.page;
     var page = Pages.findOne({slug: page_slug});
-    if(!page) return false;
+    if (page) {
+      Session.set("page-slug", page_slug); // Fixme: Abstract this away, its legacy code
+      this.template = page.template;
+    }
+    this.render();
+
+    this.render({
+      header: {to: 'header'},
+      footer: {to: 'footer'}
+    })
+  },
+  notFoundTemplate: '404',
+  data: function() {
+    return Pages.findOne({slug: this.params.page});
+  },
+  template: 'sidebar_left'
+});
+
+PageEditController = RouteController.extend({
+  run: function() {
+    var page_slug = this.params.page;
+    var page = Pages.findOne({slug: page_slug});
+    if(!page) return '404';
 
     Session.set("page-slug", page_slug);
+
+    // Add common edit page events
+    Template[page.template + '_edit'].events = {
+      'submit #pageEditForm': events.savePage,
+      'click #deletePage': events.showDeletePageModal,
+      'click .delete-page': events.deletePage
+    }
+
+    this.template = page.template + '_edit';
+
+    this.render();
+
+    this.render({
+      header: {to: 'header'},
+      footer: {to: 'footer'}
+    })
+  },
+  notFoundTemplate: '404',
+  data: function() {
+    return Pages.findOne({slug: this.params.page});
+  },
+  template: 'sidebar_left'
+});
+
+HomePageController = RouteController.extend({
+  run: function() {
+    var page_slug = utils.getSetting('indexPage');
+    var page = Pages.findOne({slug: page_slug});
+    if(!page) {
+      page = Pages.findOne();
+      if (!page) return '404';
+      else page_slug = page.slug;
+    }
+    Session.set("page-slug", page_slug);
+
     this.template = page.template;
 
     this.render();
@@ -15,9 +72,7 @@ PageController = RouteController.extend({
       footer: {to: 'footer'}
     })
   },
-  data: function() {
-    return Pages.findOne({slug: this.params.page});
-  },
+  notFoundTemplate: '404',
   template: 'sidebar_left'
 });
 
--- client/router.js
+++ client/router.js
@@ -18,14 +18,10 @@ Router.configure({
   }
 });
 
-
-
 Router.map(function() {
   this.route('loginButtonsFullPage', {path: '/login'});
 
   // Add routes for admin-level pages if user has correct permissions
-
-  //FIXME: Add 'AdminController' to these routes and deny if not logged in as admin
   this.route('site_settings', {
     controller: AdminController
   });
@@ -40,90 +36,32 @@ Router.map(function() {
     path: '/users'
   });
 
+  // Login
+  this.route('admin_users', {
+    controller: AdminController,
+    path: '/users'
+  });
+
+  // Route / to the admin-set root page or the first page
+  this.route('home', {
+    path: '/',
+    controller: HomePageController
+  })
+
+  // Route pages
+  this.route('pageEdit', {
+    path: '/:page/edit',
+    controller: PageEditController
+  })
+
   // Route pages
   this.route('page', {
     path: '/:page',
     controller: PageController
   })
 
-
-  // this.route('showPost', {
-  //   path: '/posts/:_id',
-  //   data: function() { return Posts.findOne(this.params._id); },
-  //   waitOn: postsSub,
-  //   loadingTemplate: 'loading',
-  //   notFoundTemplate: 'notFound'
-  // });
-
-
-  // this.route('showPost', {
-  //   path: '/posts/:_id',
-  //   data: function() { return Posts.findOne(this.params._id); }
-  // });
+  this.route('page', {
+    path: '*',
+    controller: PageController
+  })
 });
-/*
-Meteor.Router.add({
-
-  "/login": function() {
-    return 'loginButtonsFullPage';
-  },
-
-  "
-  "/": function() {
-    // Don't render until we have our data
-    if (!pagesSubscription.ready() || !settingsSubscription.ready()) {
-      return '';
-    } else {
-      var page_slug = utils.getSetting('indexPage');
-      var page = Pages.findOne({slug: page_slug});
-      if(!page) {
-        page = Pages.findOne();
-        if (!page) return '404';
-        else page_slug = page.slug;
-      }
-
-      Session.set("page-slug", page_slug);
-      return page.template;
-    }
-  },
-  "* / edit": function (page_slug) {
-    if (!Roles.userIsInRole(Meteor.user(), ['author','admin'])) {
-      return false;
-    }
-
-    // Don't render until we have our data
-    if (!pagesSubscription.ready() || !settingsSubscription.ready()) {
-      return '';
-    } else {
-      if (page_slug.charAt(0) == '/') page_slug = page_slug.substr(1);
-
-      var page = Pages.findOne({slug: page_slug});
-      if(!page) return '404';
-
-      // Add common edit page events
-      Template[page.template + '_edit'].events = {
-        'submit #pageEditForm': events.savePage,
-        'click #deletePage': events.showDeletePageModal,
-        'click .delete-page': events.deletePage
-      }
-
-      Session.set("page-slug", page_slug);
-      return page.template + '_edit';
-    }
-  },
-  "*": function (page_slug) {
-    // Don't render until we have our data
-    if (!pagesSubscription.ready() || !settingsSubscription.ready()) {
-      return '';
-    } else {
-      if (page_slug.charAt(0) == '/') page_slug = page_slug.substr(1);
-
-      var page = Pages.findOne({slug: page_slug});
-      if(!page) return '404';
-
-      Session.set("page-slug", page_slug);
-      return page.template;
-    }
-  }
-});
-*/
